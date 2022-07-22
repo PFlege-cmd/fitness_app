284dccbebfaf4d7a14979703735298e5c955802f
Patrick Flege <patrick.flege@rigd-loxia.nl>
2022-07-22 15:25:42 +0200

    Angular Material added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,20 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { MaterialsModule } from './materials.module';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
+    BrowserAnimationsModule,
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    MaterialsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
